4de6b97c84178b667c5cdb708b96b2509432234e
guustavomc <49313519+guustavomc@users.noreply.github.com>
2023-02-25 16:28:33 -0300

    Create Roman_to_Integer.js

---
+++ Roman_to_Integer.js
@@ -0,0 +1,56 @@
+/**
+  Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+  Symbol       Value
+  I             1
+  V             5
+  X             10
+  L             50
+  C             100
+  D             500 
+  M             1000
+  
+  Example 1:
+
+  Input: s = "III"
+  Output: 3
+  Explanation: III = 3.
+  
+  Example 2:
+
+  Input: s = "LVIII"
+  Output: 58
+  Explanation: L = 50, V= 5, III = 3.
+  
+  Example 3:
+
+  Input: s = "MCMXCIV"
+  Output: 1994
+  Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+****
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let table={
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+
+    let result=0;
+    for(let i=0; i < s.length; i++){
+        if(table[s[i]] < table[s[i+1]]){
+            result-=table[s[i]]
+        }
+        else{
+            result+=table[s[i]]
+        }
+    }
+    return result;
+};
